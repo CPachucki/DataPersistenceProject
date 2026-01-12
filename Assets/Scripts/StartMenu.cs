5bc0c655e79b1adb8218edb71e31a4a60ef3dee4
CPachucki <cpachucki@gmail.com>
2026-01-11 20:19:20 -0500

    Save Data Between Sessions
    Added functionality to save player's high score in JSON file and retrieve both name and score to display in game even when a new session is started.

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -27,22 +27,14 @@ public class StartMenu : MonoBehaviour
         if (usernameEntry.text != null)
         {
             GameManager.Instance.Username = usernameEntry.text;
+            UnityEngine.Debug.Log(GameManager.Instance.Username);
+            
             // Load game scene
             SceneManager.LoadScene(1);
         }
         else
         {
             UnityEngine.Debug.Log("Enter a username");
-        }  
-
-        
+        }          
     }
-
-    /*public void UsernameEntered(String name)
-    {
-        if (usernameEntry.text != null)
-        {
-            GameManager.Instance.Username = usernameEntry.text;
-        }        
-    }*/
 }
